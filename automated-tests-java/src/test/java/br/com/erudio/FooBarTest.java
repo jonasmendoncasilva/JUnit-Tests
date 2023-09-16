e977325368f94bd7e915c1f348814f62123c49f0
Jonas-Silva01 <140558999+Jonas-Silva01@users.noreply.github.com>
2023-09-16 18:28:38 -0400

    Inicio das Assetions, Maven Surefire adicionado

---
+++ automated-tests-java/src/test/java/br/com/erudio/FooBarTest.java
@@ -0,0 +1,13 @@
+package br.com.erudio;
+
+import org.junit.jupiter.api.Test;
+
+class FooBarTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+		System.out.println("Hello JUnit");
+	}
+
+}
---
+++ automated-tests-java/src/test/java/br/com/erudio/math/SimpleMathTest.java
@@ -0,0 +1,17 @@
+package br.com.erudio.math;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SimpleMathTest {
+
+	@Test
+	void testSum() {
+		SimpleMath math = new SimpleMath();
+		Double result = math.sum(6.2D, 2D);
+		Double expected = 8.2D;
+		
+		Assertions.assertEquals(expected, result);
+	}
+
+}
