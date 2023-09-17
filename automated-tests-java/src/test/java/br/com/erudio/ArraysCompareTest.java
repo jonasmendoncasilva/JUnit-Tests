2cc0c73c1ef689dd581369cbc400771cd0b71e64
Jonas-Silva01 <140558999+Jonas-Silva01@users.noreply.github.com>
2023-09-16 21:02:41 -0400

    Performance do método com @Timeout

--- automated-tests-java/src/test/java/br/com/erudio/ArraysCompareTest.java
+++ automated-tests-java/src/test/java/br/com/erudio/ArraysCompareTest.java
@@ -3,8 +3,10 @@ package br.com.erudio;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 class ArraysCompareTest {
 
@@ -16,5 +18,17 @@ class ArraysCompareTest {
 		Arrays.sort(numbers);
 		
 		assertArrayEquals(numbers,expectedArray);
+	}
+
+	@Test
+	//@Timeout (1)
+	@Timeout(value = 15, unit = TimeUnit.MILLISECONDS)
+	void testSortPerformance() {
+		int[] numbers = {25,8,21,32,3};
+		
+		for(int i=0;i<100000;i++) {
+			numbers[0]= i;
+			Arrays.sort(numbers);
+		}
 	}
 }
