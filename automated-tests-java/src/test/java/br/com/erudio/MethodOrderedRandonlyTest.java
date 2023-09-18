941e0ebe6abf3b4b08470b3b395cc41e8335a85a
Jonas Silva <jonasmendocasilva@gmail.com>
2023-09-18 14:34:41 -0400

    Ordem de execução aleatória dos testes

---
+++ automated-tests-java/src/test/java/br/com/erudio/MethodOrderedRandonlyTest.java
@@ -0,0 +1,30 @@
+package br.com.erudio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.Random.class)
+class MethodOrderedRandonlyTest {
+
+	@Test
+	void testA() {
+		System.out.println("Running Test A");
+	}
+	
+	@Test
+	void testB() {
+		System.out.println("Running Test A");
+	}
+	@Test
+	void testC() {
+		System.out.println("Running Test A");
+	}
+	@Test
+	void testD() {
+		System.out.println("Running Test A");
+	}
+
+}
