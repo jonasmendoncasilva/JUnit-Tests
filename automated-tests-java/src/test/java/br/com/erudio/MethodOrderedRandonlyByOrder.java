ac99e7db31487c398ad8c09a2275fffad3aaf638
Jonas-Silva01 <140558999+Jonas-Silva01@users.noreply.github.com>
2023-09-18 22:34:58 -0400

    @TestInstance para criação de apenas uma instância de testes

--- automated-tests-java/src/test/java/br/com/erudio/MethodOrderedRandonlyByOrder.java
+++ automated-tests-java/src/test/java/br/com/erudio/MethodOrderedRandonlyByOrder.java
@@ -1,34 +1,50 @@
 package br.com.erudio;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
 
-@Order(2)
+//@Order(2)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MethodOrderedRandonlyByOrder {
 
+	StringBuilder actualValue = new StringBuilder();
+	
+	@AfterEach
+	void afterEach() {
+		System.out.println("The actual value is "+ actualValue);
+	}
+	
 	@Test
 	@Order(1)
 	void testA() {
 		System.out.println("Running Test A");
+		actualValue.append("1");
 	}
 	
 	@Test
 	@Order(3)
 	void testB() {
 		System.out.println("Running Test A");
+		actualValue.append("3");
 	}
+	
 	@Test
 	@Order(4)
 	void testC() {
 		System.out.println("Running Test A");
+		actualValue.append("4");
 	}
+	
 	@Test
 	@Order(2)
 	void testD() {
 		System.out.println("Running Test A");
+		actualValue.append("2");
 	}
 
 }
