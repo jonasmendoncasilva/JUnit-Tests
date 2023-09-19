d29746aca9b494c174ccc780975f6576bb547611
Jonas Silva <jonasmendocasilva@gmail.com>
2023-09-19 14:09:24 -0400

    Inicio da programação TDD

---
+++ TDD-tests/src/test/java/br/com/erudio/PersonServiceTest.java
@@ -0,0 +1,15 @@
+package br.com.erudio;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PersonServiceTest {
+
+	@DisplayName("When Create a Person With Success Should Return a Person Object")
+	@Test
+	void testCreatePerson_WhenSucess_ShouldReturnPersonObject() {
+		//Given / Arrange
+		//When  / Act
+		//Then  / Arrest
+	}
+}
