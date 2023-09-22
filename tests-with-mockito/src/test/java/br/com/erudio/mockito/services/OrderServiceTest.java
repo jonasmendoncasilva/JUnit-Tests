749becd211133afe9531c0eb94d6b11303f9199a
Jonas Silva <jonasmendocasilva@gmail.com>
2023-09-22 17:29:08 -0400

    Mockando um método estático

---
+++ tests-with-mockito/src/test/java/br/com/erudio/mockito/services/OrderServiceTest.java
@@ -0,0 +1,32 @@
+package br.com.erudio.mockito.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mockStatic;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
+class OrderServiceTest {
+
+	OrderService service = new OrderService();
+	Object defaultUuid = UUID.fromString("8d8b30e3-de52-4f1c-a71c-9905a8043dac");
+	
+	@Test
+	@DisplayName(value="Should Include Random OrderId When No OrderId Exists")
+	void testShouldIncludeRandomOrderId_When_NoOrderIdExists() {
+		//Given
+		try(MockedStatic<UUID> mockedUuId = mockStatic(UUID.class)){
+			
+			mockedUuId.when(UUID::randomUUID).thenReturn(defaultUuid);
+		
+		//When
+		Order result = service.createOrder("MacBook", 2L, null);
+
+		//Then
+		assertEquals(defaultUuid.toString(), result.getId());
+		}
+	}
+}
---
+++ tests-with-mockito/src/test/java/br/com/erudio/mockito/services/OrderTest.java
@@ -0,0 +1,5 @@
+package br.com.erudio.mockito.services;
+
+class OrderTest{
+	
+}
