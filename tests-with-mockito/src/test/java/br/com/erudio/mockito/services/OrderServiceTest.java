619e3a7c3f91614b6c7eba8e0833e73e6c435317
Jonas-Silva01 <140558999+Jonas-Silva01@users.noreply.github.com>
2023-09-25 21:27:18 -0400

    Mock de um LocalDateTime

--- tests-with-mockito/src/test/java/br/com/erudio/mockito/services/OrderServiceTest.java
+++ tests-with-mockito/src/test/java/br/com/erudio/mockito/services/OrderServiceTest.java
@@ -3,6 +3,7 @@ package br.com.erudio.mockito.services;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mockStatic;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
@@ -12,21 +13,38 @@ import org.mockito.MockedStatic;
 class OrderServiceTest {
 
 	OrderService service = new OrderService();
-	Object defaultUuid = UUID.fromString("8d8b30e3-de52-4f1c-a71c-9905a8043dac");
-	
-	@Test
-	@DisplayName(value="Should Include Random OrderId When No OrderId Exists")
-	void testShouldIncludeRandomOrderId_When_NoOrderIdExists() {
-		//Given
-		try(MockedStatic<UUID> mockedUuId = mockStatic(UUID.class)){
-			
-			mockedUuId.when(UUID::randomUUID).thenReturn(defaultUuid);
-		
-		//When
-		Order result = service.createOrder("MacBook", 2L, null);
+	private final UUID defaultUuid = UUID.fromString("8d8b30e3-de52-4f1c-a71c-9905a8043dac");
+	private final LocalDateTime defaultLocalDateTime = LocalDateTime.of(2023, 7, 4, 5, 10);
 
-		//Then
-		assertEquals(defaultUuid.toString(), result.getId());
+	@Test
+	@DisplayName(value = "Should Include Random OrderId When No OrderId Exists")
+	void testShouldIncludeRandomOrderId_When_NoOrderIdExists() {
+		// Given
+		try (MockedStatic<UUID> mockedUuId = mockStatic(UUID.class)) {
+
+			mockedUuId.when(UUID::randomUUID).thenReturn(defaultUuid);
+
+			// When
+			Order result = service.createOrder("MacBook", 2L, null);
+
+			// Then
+			assertEquals(defaultUuid.toString(), result.getId());
+		}
+	}
+
+	@Test
+	@DisplayName(value = "Should Include Current Time When Create a New Order")
+	void testShouldIncludeCurrentTime_When_CreateANewOrder() {
+		// Given
+		try (MockedStatic<LocalDateTime> mockedUuId = mockStatic(LocalDateTime.class)) {
+
+			mockedUuId.when(LocalDateTime::now).thenReturn(defaultLocalDateTime);
+
+			// When
+			Order result = service.createOrder("MacBook", 2L, null);
+
+			// Then
+			assertEquals(defaultLocalDateTime, result.getCreationDate());
 		}
 	}
 }
